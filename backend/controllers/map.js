8cbc0611d093b5e70d25719cf66f29ac7b203d75
Jacob Feng <peanutChowder@gmail.com>
2023-12-30 02:59:47 -0700

    Import axios and google maps

--- backend/controllers/map.js
+++ backend/controllers/map.js
@@ -1,4 +1,8 @@
 const mapRouter = require('express').Router()
+const axios = require('axios')
+
+const {Client} = require("@googlemaps/google-maps-services-js")
+const client = new Client({})
 
 mapRouter.get("/", async (request, response) => {
     return response.json("hi there")

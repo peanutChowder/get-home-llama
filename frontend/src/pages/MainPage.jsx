1af9b337763f192d1c4905c482b116986471c2d0
Jacob Feng <peanutChowder@gmail.com>
2024-01-01 16:46:03 -0700

    Added temporary mobile hamburger menu

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
+import HamburgerMenu from "../components/HamburgerMenu"
+
 const MainPage = () => {
     return (
-        <>
-        </>
+        <div>
+            <HamburgerMenu/>
+            <h1>Content</h1>
+        </div>
+
     )
 }
 

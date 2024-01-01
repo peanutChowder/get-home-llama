0aa0ad4578a4ea992ebad2d7960627e4db1545cb
Jacob Feng <peanutChowder@gmail.com>
2023-12-31 18:06:49 -0700

    Added main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Main from './pages/MainPage'
+
 import './App.css'
 
 const App = () => {
   return (
-    <div></div>
+    <>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Main/>}/>
+          </Routes>
+      </Router>
+    </>
   )
 }
 

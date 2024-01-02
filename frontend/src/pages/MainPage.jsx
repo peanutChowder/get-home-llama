761f908f3c0c752e082797fb969042becd783390
Jacob Feng <peanutChowder@gmail.com>
2024-01-02 14:47:54 -0700

    Added map with curr location getter/setter

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react"
 import HamburgerMenu from "../components/HamburgerMenu"
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+
+import './MainPage.css'
 
 const MainPage = () => {
+    const [currLocation, setCurrLocation] = useState({lat: 0, lon: 0})
+    const [zoom, setZoom] = useState(1)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            setCurrLocation({
+                lat: pos.coords.latitude,
+                lon: pos.coords.longitude
+            })
+        })
+
+        if (currLocation.lat != 0 && currLocation.lon != 0) {
+            setZoom(14)
+        }
+    }, [currLocation])
+
     return (
         <div>
             <HamburgerMenu/>
-            <h1>Content</h1>
+
+            <div id="main-page-content">
+                <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                    <div className="map-container">
+                        <Map zoom={zoom} center={{lat: currLocation.lat, lng: currLocation.lon}}>
+
+                        </Map>
+                    </div>
+                </APIProvider>
+            </div>
         </div>
 
     )

39ae6de794d78a988fdbb181408d3b7f76838580
Jacob Feng <peanutchowder@gmail.com>
2024-01-10 10:55:36 -0700

    Added imports and loading libs

--- frontend/src/components/PlaceAutocomplete.jsx
+++ frontend/src/components/PlaceAutocomplete.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react"
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption
+} from "@reach/combobox"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete"
+
 const PlaceAutoComplete = () => {
-    
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+        libraries: ["places"]
+    })
+    const { 
+        ready, 
+        value, 
+        setValue, 
+        suggestions: {status, data}, 
+        clearSuggestions
+    } = usePlacesAutocomplete()
+
+    return (
+        <div>
+            <Combobox>
+                <ComboboxInput/>
+            </Combobox>
+        </div>
+    )
 }

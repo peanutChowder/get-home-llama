9c165b35979528eb49e8c61baa417fecc6a7d541
Jacob Feng <peanutchowder@gmail.com>
2024-01-16 12:48:33 -0700

    Added css

--- frontend/src/components/GoogleMap.jsx
+++ frontend/src/components/GoogleMap.jsx
@@ -2,6 +2,8 @@ import PropTypes from "prop-types"
 import Directions from "./Directions"
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps'
 
+import "./GoogleMap.css"
+
 const GoogleMap = ({ zoom, currLocation, destination, showDirections}) => {
     return (
         <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>

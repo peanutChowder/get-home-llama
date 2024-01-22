3e8c9bae11893d9751631659dc64bf0afe10c864
Jacob Feng <peanutchowder@gmail.com>
2024-01-21 17:41:26 -0700

    Add

---
+++ backend/models/secureUrl.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const secureUrlSchema = new mongoose.Schema({
+    url: String,
+    uid: String,
+    lastLocation: [Number],
+    polyline: String
+})
+
+secureUrlSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("SecureUrl", secureUrlSchema)
